89e268d8a1991f5eecabbd375d2e122586caafea
Doug Hatcher <superterran@gmail.com>
2019-04-25 22:13:26 -0400

    stubbing out letsencrypt support

--- etc/app/index.py
+++ etc/app/index.py
@@ -21,6 +21,9 @@ if config['IDE_ENABLE'] is "1":
 if config['WEB_ENABLE'] is "1":
     body += open('/app/etc/composition/web.yml.j2', 'r').read()
 
+if config['LETSENCRYPT_ENABLE'] is "1":
+    body += open('/app/etc/composition/letsencrypt.yml.j2', 'r').read()
+
 if config['DB_ENABLE'] is "1":
     body += open('/app/etc/composition/db.yml.j2', 'r').read()
 
@@ -32,6 +35,9 @@ for phpver in sorted(glob.glob('/app/etc/composition/php/*.yml.j2')):
     if ver.upper()+'_ENABLE' in config:
         if config[ver.upper()+'_ENABLE'] is "1":
             body += open(phpver, 'r').read()
+
+
+body += open('/app/etc/composition/footer.yml.j2', 'r').read()
 
 t = Template(body)
 print(t.render(config))
