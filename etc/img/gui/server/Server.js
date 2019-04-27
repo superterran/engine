9e1a734e859b88b38875246a35badd02186188b1
Doug Hatcher <superterran@gmail.com>
2019-04-27 17:12:24 -0400

    able to generate bash output in the express app

--- etc/img/gui/server/Server.js
+++ etc/img/gui/server/Server.js
@@ -1,15 +1,22 @@
-
-
+// https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+
+var cmd=require('node-cmd');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/api/docker-ps', (req, res) => {
+
+  ls = cmd.get('engine api ls', function(err, data, stderr) {
+    res.send({ 
+      output: data,
+    });
+  });
 });
 app.post('/api/world', (req, res) => {
   console.log(req.body);
@@ -20,4 +27,3 @@ app.post('/api/world', (req, res) => {
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
